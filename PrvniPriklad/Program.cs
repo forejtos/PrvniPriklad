0e94fcea1b34d647fd07b51f90b18d4b5235933b
Filip Forejt <forejt.filip.2018@skola.ssps.cz>
2021-09-07 15:09:31 +0200

    potvrzení

---
+++ PrvniPriklad/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrvniPriklad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Clovek
+    {
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+
+        public Clovek()
+        {
+            Jmeno = "Neznáme";
+            Prijmeni = "Neznáme";
+        }
+
+        public Clovek(string j, string p)
+        {
+            Jmeno = j;
+            Jmeno = p;
+        }
+
+    class Obcan : Clovek
+    {
+        int RodneCislo = 
+    }
+}
